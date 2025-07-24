97536c16a959df5ad4d17ef63dc500e7ebd89f6b
AlphaPenguino <zunder.pacis@lpunetwork.edu.ph>
2025-07-24 14:37:28 +0800

    game id creation

---
+++ frontend/app/arcade/[gameId].jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function id() {
+  return (
+    <View>
+      <Text>idtttttttt
+
+        grgrgs
+      </Text>
+    </View>
+  )
+}
